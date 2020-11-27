bcaf4607965bcc783cf623f4e09d0688e41ff5a2
Douglas Oak <ramadawn@gmail.com>
2020-11-26 22:26:45 -0800

    Code : add form code to leave review page

--- leaveReview.php
+++ leaveReview.php
@@ -13,7 +13,20 @@
 
 	
     <h1>Leave Review Page</h1>
-	
+<div class="form">
+	<form action="postReview" method="post">
+	<label>Name:</label></br><input type="text" name="name"></br>
+	<label>E-mail:</label></br> <input type="text" name="email"></br>
+	<label>Restaurant:</label></br> <input type="text" name="restaurants"></br>
+	<label>Dish:</label></br> <input type="text" name="dish"></br>
+	<label>Review:</label> </br>
+	<textarea name="review" rows="4" cols="50">
+	Write your review here
+	</textarea></br>
+	<label>Post Image:</label></br><input type="file" id="myFile" name="filename">
+	<input type="submit">
+	</form>
+</div>	
 
 	<?php include('templates/footer.php'); ?>
 </html>
